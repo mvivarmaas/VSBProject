7d2d315c7df4c777121d7c1bbb5b53946b95f86f
abubakar.daud@mail.mcgill.ca <abubakar.daud@mail.mcgill.ca>
2021-09-14 14:17:17 -0400

    heroku revert

--- mainpage/VSBLogic.py
+++ mainpage/VSBLogic.py
@@ -85,18 +85,7 @@ def get_class(class_name, term):
     url_useless = "&rq_1_0=null&nouser=1"
     url_date = get_date()
     final_url = url_base + url_term + url_course + url_useless + url_date
-
-   # r = requests.get(final_url)
-
-  # come here
-    session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    r = session.get(final_url)
-
-
+    r = requests.get(final_url)
 
 
     if r.status_code == 200:
