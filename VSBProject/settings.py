f47a888f2619670c60961adb1d299243232e752a
abubakar.daud@mail.mcgill.ca <abubakar.daud@mail.mcgill.ca>
2021-10-18 22:56:38 -0400

    commit database

--- VSBProject/settings.py
+++ VSBProject/settings.py
@@ -91,16 +91,26 @@ WSGI_APPLICATION = 'VSBProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': "django.db.backends.postgresql",
+#        "HOST": "localhost",
+#        "NAME": "adaud",
+#        "USER": "",
+#        "PORT": "5432",
+#        "PASSWORD": ""
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        "HOST": "localhost",
-        "NAME": "adaud",
-        "USER": "",
-        "PORT": "5432",
-        "PASSWORD": ""
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
